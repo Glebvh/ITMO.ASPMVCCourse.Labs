7fac284f4affb0e47c3b8ed47796fef55ec8164f
glebv <glebv@inbox.ru>
2022-10-13 11:36:29 +0300

    Add project files.

---
+++ ITMO.ASPMVCCourse.Labs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITMO.ASPMVCCourse.Labs.MvcApplication" Language="C#" %>
---
+++ ITMO.ASPMVCCourse.Labs/Models/PersonRepository.cs
@@ -0,0 +1,32 @@
+﻿using ITMO.ASPMVCCourse.Labs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASPMVCCourse.Labs.Models
+{
+    public class PersonRepository
+    {
+        private List<Person> persons = new List<Person>();
+        public int NumberOfPerson
+        { 
+            get 
+            { 
+                return persons.Count(); 
+            }
+        }
+        public IEnumerable<Person> GetAllResponses
+        {
+            get
+            {
+                return persons;
+            }
+        }
+        public void AddResponse(Person pers) 
+        { 
+            persons.Add(pers);
+        }
+
+    }
+}
